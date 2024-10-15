3008785cbf4836f6517a17738c17b02218932905
HernanDuque.dukeherrera73@gmail.com <51803616+hernanduque@users.noreply.github.com>
2024-10-15 15:11:47 -0500

    Update ApartamentoVisitumController.cs
    adicion de metodos Guardar, Editar y eliminar

--- AgenciaArriendo.Server/AgenciaArriendo.Server/Controllers/ApartamentoVisitumController.cs
+++ AgenciaArriendo.Server/AgenciaArriendo.Server/Controllers/ApartamentoVisitumController.cs
@@ -106,23 +106,118 @@ namespace AgenciaArriendo.Server.Controllers
             {
                 var TableApartamentoVisita = new TableApartamentoVisitum
                 {
-
+                    Strapartamentovisita = apartamentoVisitum.Strapartamentovisita,
+                    Strestado = apartamentoVisitum.Strestado,
+                    Strcodigosector = apartamentoVisitum.Strcodigosector,
                 };
+                    _dbContext.TableApartamentoVisita.Add(TableApartamentoVisita);
+                    await _dbContext.SaveChangesAsync();
+
+    if (TableApartamentoVisita.Strcodigo != null)
+    {
+        responseApi.Escorrecto = true;
+        responseApi.Valor = TableApartamentoVisita.Strcodigo;
+    }
+    else
+    {
+        responseApi.Escorrecto = false;
+        responseApi.Mensaje = "No guardado";
+    }
+
+}
 
 
+catch (Exception ex)
+{
+    responseApi.Escorrecto = false;
+    responseApi.Mensaje = ex.Message;
+    throw;
+}
+return Ok(responseApi);
+}
 
-                responseApi.Escorrecto = true;
-                responseApi.Valor = ApartamentoVisitumDTO;
-            }
+[HttpPut]
+[Route("Editar/{Strcodigo}")]
+public async Task<IActionResult> Editar(ApartamentoVisitumDTO apartamentoVisitum, String Strcodigo)
+{
+    var responseApi = new ResponseAPI<String>();
+
+    try
+    {
+        
+        var TableApartamentoVisita = await _dbContext.TableApartamentoVisita.FirstOrDefaultAsync(e => e.Strcodigo == Strcodigo);
+        await _dbContext.SaveChangesAsync();
+
+        if (TableApartamentoVisita != null)
+        {                    
+            //TableApartamentoVisita.Strcodigo = apartamentoVisitum.Strcodigo;
+            TableApartamentoVisita.Strapartamentovisita = apartamentoVisitum.Strapartamentovisita;
+            TableApartamentoVisita.Strestado = apartamentoVisitum.Strestado;
+            TableApartamentoVisita.Strcodigosector = apartamentoVisitum.Strcodigosector;
 
 
-            catch (Exception ex)
-            {
-                responseApi.Escorrecto = false;
-                responseApi.Mensaje = ex.Message;
-                throw;
-            }
-            return Ok(responseApi);
+            _dbContext.TableApartamentoVisita.Update(TableApartamentoVisita);
+            await _dbContext.SaveChangesAsync();
+            responseApi.Escorrecto = true;
+            responseApi.Valor = TableApartamentoVisita.Strcodigo;
+
+           
         }
+        else
+        {
+            responseApi.Escorrecto = false;
+            responseApi.Mensaje = "Apartamento no encontrado";
+        }
+
+    }
+
+
+    catch (Exception ex)
+    {
+        responseApi.Escorrecto = false;
+        responseApi.Mensaje = ex.Message;
+        throw;
+    }
+    return Ok(responseApi);
+}
+
+[HttpDelete]
+[Route("Eliminar/{Strcodigo}")]
+public async Task<IActionResult> Eliminar(String Strcodigo)
+{
+    var responseApi = new ResponseAPI<String>();
+
+    try
+    {
+
+        var TableApartamentoVisita = await _dbContext.TableApartamentoVisita.FirstOrDefaultAsync(e => e.Strcodigo == Strcodigo);
+        await _dbContext.SaveChangesAsync();
+
+        if (TableApartamentoVisita != null)
+        {
+            
+            _dbContext.TableApartamentoVisita.Update(TableApartamentoVisita);
+            await _dbContext.SaveChangesAsync();
+
+            responseApi.Escorrecto = true;
+           
+        }
+        else
+        {
+            responseApi.Escorrecto = false;
+            responseApi.Mensaje = "Apartamento no encontrado";
+        }
+
+    }
+
+
+    catch (Exception ex)
+    {
+        responseApi.Escorrecto = false;
+        responseApi.Mensaje = ex.Message;
+        throw;
+    }
+    return Ok(responseApi);
+}
     }
 }
